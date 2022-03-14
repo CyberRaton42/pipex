af903fe2906c4108c43402059edfd70fc3395d3c
CyberRaton <hbembnis@student.42.fr>
2022-03-14 15:51:44 +0100

    pipex start

---
+++ pipex.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbembnis <hbembnis@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 15:29:26 by hbembnis          #+#    #+#             */
+/*   Updated: 2022/03/14 15:34:21 by hbembnis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
