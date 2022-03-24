07dc1b127f0dd153283d68d366da6e81bf88fa72
Swan <hugo.bembnista@gmail.com>
2022-03-24 13:58:43 +0100

    ça marche toujours pas

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,26 +6,11 @@
 /*   By: hbembnis <hbembnis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 15:27:25 by hbembnis          #+#    #+#             */
-/*   Updated: 2022/03/21 17:19:42 by hbembnis         ###   ########.fr       */
+/*   Updated: 2022/03/24 00:37:38 by hbembnis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-int	first_cmd(char **argv, char **envp, int *pipefd)
-{
-	int	infile_fd;
-
-	infile_fd = open(argv[1], O_RDONLY);
-	if (infile_fd == -1)
-		ft_error();
-	dup2(pipefd[1], STDOUT_FILENO);
-	dup2(infile_fd, STDIN_FILENO);
-	close(pipefd[0]);
-	if (exec_cmd(argv[2], envp) == 0)
-		return (0);
-	return (1);
-}
 
 int	second_cmd(char **argv, char **envp, int *pipefd)
 {
@@ -42,11 +27,48 @@ int	second_cmd(char **argv, char **envp, int *pipefd)
 	return (1);
 }
 
+int	first_cmd(char **argv, char **envp, int *pipefd)
+{
+	int		infile_fd;
+	//pid_t	pid2;
+	
+	/*pid2 = fork();
+	if (pid2 == -1)
+		ft_error();
+	if (pid2 == 0)
+	{
+		if (second_cmd(argv, envp, pipefd) == 0)
+			return (0);
+	}
+	else
+	{*/
+	infile_fd = open(argv[1], O_RDONLY);
+	if (infile_fd == -1)
+		ft_error();
+	dup2(pipefd[1], STDOUT_FILENO);
+	dup2(infile_fd, STDIN_FILENO);
+	close(pipefd[0]);
+	if (exec_cmd(argv[2], envp) == 0)
+			return (0);
+	//}
+	return (1);
+}
+
+void	ft_close(int *pipefd)
+{
+	close(pipefd[0]);
+	close(pipefd[1]);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	int		pipefd[2];
 	pid_t	pid1;
+	pid_t	pid2;
+	//int i;
 
+	//i = 0;
+	//i++;
 	if (argc == 5)
 	{
 		if (pipe(pipefd) == -1)
@@ -56,14 +78,24 @@ int	main(int argc, char **argv, char **envp)
 			ft_error();
 		if (pid1 == 0)
 		{
+			//ft_putstr_fd("child1", 2);
 			if (first_cmd(argv, envp, pipefd) == 0)
 				return (127);
 		}
+		//i = read(pipefd[0], "", 0);
+		pid2 = fork();
+		if (pid2 == -1)
+			ft_error();
+		if (pid2 == 0)
+		{
+			//ft_putstr_fd("child2", 2);
+			if (second_cmd(argv, envp, pipefd) == 0)
+				return (127);
+		}
 		waitpid(pid1, NULL, 0);
-		if (second_cmd(argv, envp, pipefd) == 0)
-			return (127);
+		ft_close(pipefd);
 	}
 	else
 		arg_error();
-	return (1);
+	return (0);
 }
